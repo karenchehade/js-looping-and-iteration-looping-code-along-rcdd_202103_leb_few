adb5734681a0ff62012f9b5f5b3fc071f9c9886d
karen chehade <karen_chehade@hotmail.com>
2021-04-10 11:10:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(arr_str , name){
+  for(let i=0 ; i< arr_str.length ; i++){
+    console.log(`Thank you, ${arr_str[i]} , for the wonderful ${name} gift!`);
+
+  }
+    //arr_str.splice(0,3);
+
+    return arr_str;
+}
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+function countDown(int_number) {
+  if(int_number > 0){
+while (int_number >= 0) {
+      console.log(int_number);
+      int_number--;
+    }
+  }
+}
+countDown(10);
